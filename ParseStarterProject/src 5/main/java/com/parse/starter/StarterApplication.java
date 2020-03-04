260e4ef8cd47f286b0b0bbde1669155e73aa6e2d
chien314 <chienyi2@illinois.edu>
2020-03-04 16:59:56 -0600

    Initial commit

---
+++ ParseStarterProject/src 5/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // e6uPFYEglpBE
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("d67849728c68e0210f9d9a23bab4e99250e70c6e")
+            .clientKey("edf2145bc411b45432fa1b1a88de78a33f2fd641")
+            .server("http://3.19.70.239:80/parse/")
+            .build()
+    );
+
+
+
+    //ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
