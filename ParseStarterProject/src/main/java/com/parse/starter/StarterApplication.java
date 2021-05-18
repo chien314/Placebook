7acaeaf1ed8f98372710efe7e24fe0744ecca6a7
chien314 <chienyi2@illinois.edu>
2021-05-18 10:42:53 -0500

    update

---
+++ ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // e6uPFYEglpBE
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("d67849728c68e0210f9d9a23bab4e99250e70c6e")
+            .clientKey("edf2145bc411b45432fa1b1a88de78a33f2fd641")
+            .server("http://18.221.44.117:80/parse/")
+            .build()
+    );
+
+//    ParseObject object = new ParseObject("ExampleObject1");
+//    object.put("myNumber1", "1234");
+//    object.put("myString1", "rob1");
+//
+//    object.saveInBackground(new SaveCallback() {
+//      @Override
+//      public void done(ParseException ex) {
+//        if (ex == null) {
+//          Log.i("Parse Result", "Successful!");
+//        } else {
+//          Log.i("Parse Result", "Failed" + ex.toString());
+//        }
+//      }
+//    });
+//
+//    ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Placebook (Mobile APP development in Java)
 Placebook is an Mobile APP that allows users to leave messages for places they visited, which are also accessible by other users. The backend is built using AWS EC2 cloud infrastructure. This APP is developed in Java using Android Studio.
-
